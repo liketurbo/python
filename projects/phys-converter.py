cb3f05d52a0201a49e8f2bd1143c707a91c5dbdf
Ramzan Chasygov <theliketurbo@gmail.com>
2019-05-06 21:10:58 +0300

    implemented phys-converter

---
+++ projects/phys-converter.py
@@ -0,0 +1,18 @@
+def inch_meter(inches):
+  return (inches * 2.54) * 100
+
+
+def mile_kilometr(miles):
+  return miles * 1.609
+
+
+def kilometr_mile(kilometres):
+  return kilometres / 1.609
+
+
+def fahrenheit_celsius(fahrenheit):
+  return (fahrenheit - 32) * (5 / 9)
+
+
+def celsius_fahrenheit(celsius):
+  return (celsius * (9 / 5)) + 32
